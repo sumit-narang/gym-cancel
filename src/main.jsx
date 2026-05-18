8e9ee7ff3510a59b1ec97509efd104c29e11011a
Sumit Narang <sumitnarang.web@gmail.com>
2026-05-18 19:52:19 +0100

    Fix base path for subpath deployment and add GoatCounter analytics

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/gymcancel">
+    <BrowserRouter basename="/gym-cancel">
       <App />
     </BrowserRouter>
   </StrictMode>,
